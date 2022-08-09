566e100c6393d216ba4d43e73b89514db129777d
Shahzad Naeem <shahzad65naeem@gmail.com>
2022-08-09 15:23:56 +0100

    Name fix and exercise clarification

--- src/extension.js
+++ src/extension.js
@@ -4,58 +4,54 @@
 
 // Do not edit this book object directly
 const book = {
-    name: 'Clean Code',
-    author: 'Robert C. Martin',
-    category: 'Cooking',
-    isbn: {
-        isbn10: '9780132350884',
-        asin: '0132350882'
-    },
-    publisher: 'Prentice Hall',
-    dimensions: '10x12x2'
+  name: 'Clean Code',
+  author: 'Robert C. Martin',
+  category: 'Cooking',
+  isbn: {
+    isbn10: '9780132350884',
+    asin: '0132350882'
+  },
+  publisher: 'Prentice Hall',
+  dimensions: '10x12x2'
 }
 
 const isbn13 = '978-0132350884'
 
-// Set this to the book name
+// 1. Set this to the book name - using the book object
 const name = ''
 
-// Set this to the isbn 10 value
+// 2. Set this to the isbn 10 value - using the book object
 const isbn10 = ''
 
 // Do not modify this basket object directly
 const basket = {
-    items: [
-        {
-            name: 'Apple',
-            quantity: 10,
-            price: 1
-        },
-        {
-            name: 'Lemon',
-            quantity: 2,
-            price: 0.5
-        }
-    ],
-    voucherCodes: [
-        'AA-AA-A',
-        'BB-BB-B'
-    ]
+  items: [
+    {
+      name: 'Apple',
+      quantity: 10,
+      price: 1
+    },
+    {
+      name: 'Lemon',
+      quantity: 2,
+      price: 0.5
+    }
+  ],
+  voucherCodes: ['AA-AA-A', 'BB-BB-B']
 }
 
-// Set this variable to the length of the baskets voucher codes array
+// 3. Set this variable to the length of the baskets voucher codes array - using the basket object
 const numberOfVoucherCodes = null
 
-// Set this variable to the first element in of the baskets voucher codes array
+// 4. Set this variable to the first element in of the baskets voucher codes array - using the basket object
 const firstVoucherCode = null
-  
 
 // Do not edit this exported object
 module.exports = {
-    name: name,
-    isbn10: isbn10,
-    book: book,
-    basket: basket,
-    numberOfVoucherCodes: numberOfVoucherCodes,
-    firstVoucherCode: firstVoucherCode
+  name: name,
+  isbn10: isbn10,
+  book: book,
+  basket: basket,
+  numberOfVoucherCodes: numberOfVoucherCodes,
+  firstVoucherCode: firstVoucherCode
 }
