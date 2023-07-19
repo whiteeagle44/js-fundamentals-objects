7dcd3c35d9b5d3fc16705cbe4981511ff6158b64
Marcin Jankowski <marcin.jankowski@talan.com>
2023-07-19 11:48:14 +0200

    solve task

--- src/core.js
+++ src/core.js
@@ -2,32 +2,49 @@
 const person = { age: 32, size: 'Small' }
 
 // 1. Give the person object a name property with the value Matt
+person.name = "Matt"
 
 // 2. Remove the size property from the person
+delete person.size
 
 // 3. Increase the person's age by 11
+person.age += 11
 
 // 4. Add an instruments property to the person, initialised as an empty array
+person.instruments = []
 
 // 5. Add the following instruments to the persons instruments array: Guitar, Piano, Vocals
+person.instruments.push("Guitar", "Piano", "Vocals")
 
 // 6. Using an index on the instruments array, set the mainInstrument variable below
 // to the third instrument in the array
-const mainInstrument = undefined
+const mainInstrument = person.instruments[2]
 
 // 7. Add a profession property to the person, which is an object
+person.profession = { }
 
 // 8. Add a name property to the profession object with the value Musician
+person.profession.name = "Musician"
 
 // 9. Add a friends property to the person, which is an empty array
+person.friends = []
 
 // 10. Add two objects to the persons friends array with the following properties:
 // Friend one: name - Chris, age - 46
 // Friend two: name - Dom, age - 43
+person.friends.push({ 
+  name: "Chris",
+  age: 46
+})
+
+person.friends.push({
+  name: "Dom", 
+  age: 43
+})
 
 // 11. Using an index on the persons friends array, set the bestFriend variable below
 // to the name of the first friend in the array
-const bestFriend = undefined
+const bestFriend = person.friends[0].name
 
 // Don't change the code below this line
 module.exports = {
--- src/extension.js
+++ src/extension.js
@@ -19,10 +19,16 @@ const book = {
 const isbn13 = '978-0132350884'
 
 // 1. Set this to the book name - using the book object
-const name = ''
+const name = book.name
 
 // 2. Set this to the isbn 10 value - using the book object
-const isbn10 = ''
+const isbn10 = book.isbn.isbn10
+
+book.pages = 464
+book.isbn.isbn13 = '978-0132350884'
+delete book.dimensions
+book.category = 'Programming'
+delete book.isbn.asin
 
 // Do not modify this basket object directly
 const basket = {
@@ -42,10 +48,13 @@ const basket = {
 }
 
 // 3. Set this variable to the length of the baskets voucher codes array - using the basket object
-const numberOfVoucherCodes = null
+const numberOfVoucherCodes = basket.voucherCodes.length
 
 // 4. Set this variable to the first element in of the baskets voucher codes array - using the basket object
-const firstVoucherCode = null
+const firstVoucherCode = basket.voucherCodes[0]
+
+basket.items[0].price = 2
+basket.items.push({ name: 'Oranges', quantity: 4, price: 0.75 })
 
 // Do not edit this exported object
 module.exports = {
